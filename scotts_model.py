093ae1826f98b4b320b23b02dc6b2144f4b102ab
Erin Burnside <eburnsid@gmail.com>
2014-07-17 12:07:17 -0700

    function to compute first half of score

--- scotts_model.py
+++ scotts_model.py
@@ -1,5 +1,6 @@
 import psycopg2
 
+
 def get_cpc (pat_num, table = "cpcs"):
     conn = psycopg2.connect(database = 'patents', user = 'postgres')
     cur = conn.cursor()
@@ -7,3 +8,23 @@ def get_cpc (pat_num, table = "cpcs"):
     query = 'SELECT * FROM %s WHERE pat_num = %s;' % (table, '%s')
     cur.execute(query, [pat_num])
     return [code[1] for code in cur.fetchall()]
+
+
+def score_first_half (cpc_half_1, cpc_half_2):
+    score = 0
+    
+    for chs in zip(cpc_half_1[0:4], cpc_half_2[0:4]):
+        if chs[0] == chs[1]:
+            score += 10
+        else:
+            break
+            
+    if score == 40 and cpc_half_1[4:] == cpc_half_2[4:]:
+        return score + 30
+    elif score == 40:
+        return score + 20
+    return score
+
+
+if __name__ == "__main__":
+    pass
