b3233ab4348946390c09fadcffd28c6052d3ec30
Erin Burnside <eburnsid@gmail.com>
2014-07-16 18:14:42 -0700

    implements functions for adding tables with scraped patent data

--- sql_structures.py
+++ sql_structures.py
@@ -1,9 +1,27 @@
 '''
-This module is for converting existing .csv files containing patent data into usable SQL tables in the 'patents' database. These tables will include:
+This module is for converting existing .csv files containing patent data into usable SQL tables 
+in the 'patents' database. These tables will include:
 
-* 'cpcs': a table mapping all patent numbers to their respective Cooperative Patent Classification (CPC) codes, with one row for each pairing of patent number to code and each patent number having at least one but likely many codes
-* 'experts': a table mapping experts to the patents on which they have previously testified, with one row for each pairing of expert to patent and the possibility of seeing both experts and patents repeated
-* 'exp_cpcs': a subset of 'cpcs' that will contain only those patent numbers that are currently tied to an expert witness
+* 'cpcs': A table mapping all patent numbers to their respective Cooperative Patent 
+    Classification (CPC) codes, with one row for each pairing of patent number to 
+    code and each patent number having at least one but likely many codes. Table initially
+    created in psql with query "CREATE TABLE cpcs(pat_num INT, cpc TEXT);" and built out with
+    function add_to_cpcs.
+* 'experts': A table mapping experts to the patents on which they have previously testified, 
+    with one row for each pairing of expert to patent and the possibility of seeing both 
+    experts and patents repeated. Created by calling add_to_experts.
+* 'exp_cpcs': A subset of 'cpcs' that will contain only those patent numbers that are 
+    currently tied to an expert witness. Not created in this module but included here for 
+    reference. Created in psql with query "CREATE TABLE exp_cpcs AS (SELECT * FROM cpcs 
+    WHERE pat_num IN (SELECT pat_num FROM experts));"
+* 'inventors': A table mapping all experts in the database to each patent on which they have
+    previously testified. Many experts have testified on multiple patents, and some patents
+    have more than one expert.
+* 'patent_text': A table containing all text data for each patent associated with an expert,
+    which includes its title, abstract, all claims and its description.
+* 'references': A table mapping all patents with an associated expert to each of the patents
+    that they cite as references. Most patents have multiple references, and some references may 
+    occur multiple times.
 
 '''
 
@@ -12,9 +30,13 @@ import pandas as pd
 import psycopg2
 import csv
 from sqlalchemy import create_engine
+import patent_scraper
 
 
 def add_to_cpcs (data_file, cutoff):
+    conn = psycopg2.connect(database = 'patents', user = 'postgres')
+    cur = conn.cursor()
+    
     with open(data_file, 'r') as csvfile:
         pat_reader = csv.reader(csvfile, delimiter = '|')
         ii = 0
@@ -24,7 +46,7 @@ def add_to_cpcs (data_file, cutoff):
                 for jj in range(1, len(row) - 1):
                     query = "INSERT INTO cpcs (pat_num, cpc) VALUES (%s, %s);"
                     cur.execute(query,(pat_num, row[jj]))
-				conn.commit()
+                conn.commit()
             ii += 1    
 
 
@@ -42,27 +64,50 @@ def create_ctp (ctp_data_file):
     return df_ctp
 
 
-def add_to_experts (ctp_data_file, etc_data_file):
+def add_to_experts (ctp_data_file, etc_data_file, engine):
     df_ctp = create_ctp(ctp_data_file)
     df_etc = pd.read_csv(etc_data_file)
     
     df = df_ctp.merge(df_rtc, left_on = 'case_num', right_on = 'case')
     df.drop('case', axis = 1, inplace = True)
     
-    engine = create_engine('postgresql://erinburnside:@localhost/patents')
     df.to_sql('experts', engine)
+    
+    return df_ctp
+    
 
+def use_scraper(df, pat_column):
+    pat_list = df[pat_column].tolist()
+    df['titles'], df['inventors'], df['refs'], df['abstr'], df['claims'], df['descr'] = patent_scraper.scrape_patents(pat_list)
+    
+
+def create_one_to_many (df, one_col, many_col):
+    otm_list = []
+    
+    for ii, one in enumerate(df[one_col]):
+        for many in df[many_col].iloc[ii]:
+            otm_list.append([one, many])
+        
+    df_otm = pd.DataFrame(otm_list)
+    return df_otm
+
+    
+def add_pat_text_tables(df, pat_column, engine):
+    use_scraper(df, pat_column) 
+    
+    df_text = df[['pat_num','titles','abstr','claims','descr']]
+    df_text.to_sql('patent_text', engine)
+    
+    create_one_to_many(df, 'pat_num', 'inventors').to_sql('inventors', engine)
+    create_one_to_many(df, 'pat_num', 'refs').to_sql('references', engine)
+    
                
 if __name__ == '__main__':
-	conn = psycopg2.connect(database = 'patents', user = 'postgres')
-	cur = conn.cursor()
-
-	cur.execute("CREATE TABLE cpcs(pat_num INT, cpc TEXT);")
-	conn.commit()
-
-	add_to_cpcs('data/pat_to_cpc_mini.csv', 4291218)
-    add_to_experts('data/case_to_pat.csv', 'data/record_to_case.csv')
+    add_to_cpcs('data/pat_to_cpc_mini.csv', 4291218)
     
-    exp_cpc_query = "CREATE TABLE exp_cpcs AS (SELECT * FROM cpcs WHERE pat_num IN (SELECT pat_num FROM experts));"
-    cur.execute(exp_cpc_query)
-    conn.commit()    
+    engine = create_engine('postgresql://erinburnside:@localhost/patents')
+    df_ctp = add_to_experts('data/case_to_pat.csv', 'data/record_to_case.csv', engine)
+    
+    add_pat_text_tables(df_ctp, 'pat_num', engine)
+    
+    
